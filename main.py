20818953433800c93edbc7cbe956c9384533800f
Jobertote <jobdorado15@gmail.com>
2026-07-22 13:28:15 -0600

    add load csv to process

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from src.utils.paths import get_path_raw
 from src.extract.extract import extract
 from src.transform.clean_df import clean_heads, keep_columns
-from src.transform.transform import get_default_calculations
+from src.load.load import generate_clean_csv
 
 
 def run_pipeline():
-    df = get_default_calculations(keep_columns(clean_heads(extract(get_path_raw("dataset.csv")))))
+    df = keep_columns(clean_heads(extract(get_path_raw("dataset.csv"))))
+    generate_clean_csv(df.head(100))
     print(df)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    prueba()
+    run_pipeline()
--- src/load/load.py
+++ src/load/load.py
@@ -1,1 +1,5 @@
-    
+import pandas as pd
+from src.utils.paths import get_path_processed
+
+def generate_clean_csv(df: pd.DataFrame):
+    df.to_csv(get_path_processed("clean_dataset.csv"), index=False)
